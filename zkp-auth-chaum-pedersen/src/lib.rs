44d7db23796dc2d9fbeb64f0ab1252063f306d7d
lufa23 <usuario@mind-your-business.whatever>
2024-04-01 20:02:06 +0200

    added random number gen

--- zkp-auth-chaum-pedersen/src/lib.rs
+++ zkp-auth-chaum-pedersen/src/lib.rs
@@ -1,5 +1,5 @@
-use num_bigint::BigUint;
-
+use num_bigint::{BigUint, RandBigInt};
+use rand;
 
 /// output = n^exp mod p
 pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
@@ -24,21 +24,29 @@ pub fn verify(r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, alpha: &Bi
     cond1 && cond2
 }
 
+
+pub fn generate_random_below(bound: &BigUint) -> BigUint {
+    let mut rng = rand::thread_rng();
+
+    rng.gen_biguint_below(bound)
+
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_example() {
+    fn test_example_with_random_numbers() {
         let alpha = BigUint::from(4u32);
         let beta = BigUint::from(9u32);
         let p = BigUint::from(23u32);
         let q = BigUint::from(11u32);
 
         let x = BigUint::from(6u32);
-        let k = BigUint::from(7u32);
+        let k = generate_random_below(&q);
 
-        let c = BigUint::from(4u32);
+        let c = generate_random_below(&q);
 
         let y1 = exponentiate(&alpha, &x, &p);
         let y2 = exponentiate(&beta, &x, &p);
@@ -48,21 +56,13 @@ mod test {
         let r1 = exponentiate(&alpha, &k, &p);
         let r2 = exponentiate(&beta, &k, &p);
 
-        assert_eq!(r1, BigUint::from(8u32));
-        assert_eq!(r2, BigUint::from(4u32));
+
         
         let s = solve(&k, &c , &x, &q);
-        assert_eq!(s, BigUint::from(5u32));
 
         let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
         assert!(result);
 
-        // fake secret
-        let x_fake = BigUint::from(7u32);
-        let s_fake = solve(&k, &c , &x_fake, &q);
-        assert_eq!(s, BigUint::from(5u32));
 
-        let result = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s_fake, &p);
-        assert!(!result);
     }
 }
