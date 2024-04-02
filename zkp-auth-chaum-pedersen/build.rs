cf494aea2c50aed07c9b3b9a54e7d7d4e112bec2
lufa23 <usuario@mind-your-business.whatever>
2024-04-02 13:30:38 +0200

    added grpc

---
+++ zkp-auth-chaum-pedersen/build.rs
@@ -0,0 +1,12 @@
+use tonic_build;
+
+fn main() {
+    tonic_build::configure()
+        .build_server(true)
+        .out_dir("src/")
+        .compile(
+            &["proto/zkp_auth.proto"],
+            &["proto/"],
+        )
+        .unwrap();
+}
